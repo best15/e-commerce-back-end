871303f136c45729bbfa1266b8e9d2ee05db4821
SBC <lochan.sharad@gmail.com>
2021-06-11 22:01:03 +0800

    Changes in category-routes.js file. Code to get category getbyid, create category, update category and delete category added.

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -12,32 +12,76 @@ router.get('/', async (req, res) => {
     });
     res.status(200).json(categoryData);
   } catch (error) {
+    res.status(500).json(error);
+  }
+
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No Category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+
+});
+
+router.post('/', async (req, res) => {
+  // create a new category
+  try {
+    const newCategoryData = await Category.create(req.body);
+    res.status(200).json(newCategoryData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No Category with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No library card found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
     res.status(500).json(err);
   }
-  // find all categories
-  // be sure to include its associated Products
-});
-
-router.get('/:id', (req, res) => {
-  try {
-
-  } catch (error) {
-
-  }
-  // find one category by its `id` value
-  // be sure to include its associated Products
-});
-
-router.post('/', (req, res) => {
-  // create a new category
-});
-
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
-
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
 });
 
 module.exports = router;
